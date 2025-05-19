c2259de2b410c975ce5ef2ffdfbef38f8d877d5f
FICORE TOOLS SUITE <ficore.ai.africa@gmail.com>
2025-05-19 14:48:13 +0100

    Update bill.py

--- blueprints/bill.py
+++ blueprints/bill.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, session, redirect, url_for, render_template, flash
 from json_store import JsonStorageManager
-from sendgrid_email import send_email
+from mailersend_email import send_email
 from datetime import datetime
 
 bill_bp = Blueprint('bill', __name__)
