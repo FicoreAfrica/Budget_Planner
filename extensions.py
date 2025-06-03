8bd1722de405343d561982cecf43fe31dd0dd333
FICORE TOOLS SUITE <ficore.ai.africa@gmail.com>
2025-06-04 00:09:46 +0100

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -3,7 +3,7 @@ from flask_login import LoginManager
 from flask_session import Session
 from flask_wtf.csrf import CSRFProtect
 
-     db = SQLAlchemy()
-     login_manager = LoginManager()
-     session = Session()
-     csrf = CSRFProtect()
+db = SQLAlchemy()
+login_manager = LoginManager()
+session = Session()
+csrf = CSRFProtect()
