e0c5dd30d646172b1580ce49367170fa37099fd5
FICORE TOOLS SUITE <ficore.ai.africa@gmail.com>
2025-06-04 02:40:27 +0100

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
+from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 from datetime import datetime  # Ensure datetime is imported
 
